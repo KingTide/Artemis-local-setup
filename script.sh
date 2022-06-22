0a52186a49b3f52bf44f09dd347403a1f0a64604
Daniel Zamanillo <dzamanillok@gmail.com>
2022-06-22 10:11:55 -0500

    testing curl

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash/
+
+echo "test test test"
+
+# echo "Installing Homebrew"
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# sleep 2
+
+# echo "Installing Node and NPM"
+
+# curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+
+# sleep 2
+
+# echo "Installing Yarn"
+
+# brew install yarn
