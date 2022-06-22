2ae0a477e4ab4277daeeab526e89fc8b9ca9802f
Daniel Zamanillo <dzamanillok@gmail.com>
2022-06-22 12:43:40 -0500

    script: add username and pathing

--- script.sh
+++ script.sh
@@ -1,18 +1,45 @@
 #!/bin/bash/
 
+echo "Please enter your computer username"
+read USERNAME
+
+
 echo "Installing Homebrew"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USERNAME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 sleep 2
 
 echo "Installing Node and NPM"
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+brew install node@16
+
+echo 'export PATH="/opt/homebrew/opt/node@16/bin:$PATH"' >> ~/.zshrc
 
 sleep 2
 
 echo "Installing Yarn"
 
 brew install yarn
+
+sleep 2
+
+echo "Testing download"
+
+echo "Homebrew"
+brew -v
+echo "Node"
+node -v 
+echo "NPM"
+npm -v
+echo "Yarn"
+yarn -v
+
+echo "If you see versions, you have successfully installed Homebrew, Node, NPM, and Yarn"
+
+sleep 1
+
+echo "COMPLETE"
