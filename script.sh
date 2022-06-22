5fb96560cc6250a04ec988e323e80a65012632f5
Daniel Zamanillo <dzamanillok@gmail.com>
2022-06-22 14:07:54 -0500

    script: remove sudo

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Homebrew"
 
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
