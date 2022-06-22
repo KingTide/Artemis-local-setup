2333e611a0c561b9c876412c5de605af5bd58dd7
Daniel Zamanillo <dzamanillok@gmail.com>
2022-06-22 10:17:53 -0500

    updated readme and added install commands

---
+++ README.md
@@ -0,0 +1,13 @@
+# Artemis Local Script
+
+This script will install
+
+- Homebrew
+- Node 16 and NPM
+- Yarn
+
+## Install Command
+
+Copy and paste the following into your terminal
+
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/KingTide/artemis-local-setup/HEAD/script.sh)"`
--- script.sh
+++ script.sh
@@ -1,20 +1,18 @@
 #!/bin/bash/
 
-echo "test test test"
+echo "Installing Homebrew"
 
-# echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sleep 2
 
-# sleep 2
+echo "Installing Node and NPM"
 
-# echo "Installing Node and NPM"
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-# curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-# sudo apt-get install -y nodejs
+sleep 2
 
-# sleep 2
+echo "Installing Yarn"
 
-# echo "Installing Yarn"
-
-# brew install yarn
+brew install yarn
